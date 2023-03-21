414331aa66e9e1b52c83c46b47dea27a86c5653a
MJ <75807617+kang123077@users.noreply.github.com>
2023-03-21 18:27:02 +0900

    Feature/basic system initialize (#7)
    * Add: Making Test Folder
    
    #3
    + 테스트 폴더 만듬
    
    * BasicManager setting
    
    * Add : Basic Asset Prefab
    
    #3
    + 프리팹 추가
     (마일스, 정찰형 로봇, 역병의사,
      집정관, 자폭형 로봇)
    
    ---------
    
    Co-authored-by: KongJin <pibrod@gmail.com>
    Co-authored-by: hwanny1011 <86943651+Hehwanny@users.noreply.github.com>

---
+++ MarstoEarth/Assets/Scripts/OutGameManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutGameManager : Singleton<ResourceManager>
+{
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+
+    
+}
---
+++ MarstoEarth/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager :Singleton<ResourceManager>
+{
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+
+    
+}
