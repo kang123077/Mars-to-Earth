03a7e896ba574be499fda3cd9c47bea131f81603
KongJin <pibrod@gmail.com>
2023-03-29 15:32:37 +0900

    Add kamikaze
    #9
     + 몬스터 클래스를 상속받도록 cr42클래스 분리
     + 몬스터를 상속받는 kamikaze클래스 추가

---
+++ MarstoEarth/Assets/Scripts/Character/Console.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Character
+{
+    public class Console : Character
+    {
+        [SerializeField] private float optanium;
+        [SerializeField] private float experience;
+
+        
+    }
+}
