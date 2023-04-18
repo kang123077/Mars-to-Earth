278117bba605beb9c5696951d44e132420c030c2
hwanny1011 <86943651+Hehwanny@users.noreply.github.com>
2023-04-18 16:11:12 +0900

    Fix : Error
    박스 콜라이더 삭제 시 에러 발생
    >> 몬스터가 죽지 않음
    
    구르기 에러

--- MarstoEarth/Assets/Scripts/Controller/CameraController.cs
+++ MarstoEarth/Assets/Scripts/Controller/CameraController.cs
@@ -1,24 +1,30 @@
-using Character;
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
     void LateUpdate()
     {
-        Vector3 direction = (SpawnManager.Instance.playerTransform.position - new Vector3(0, 1f, 0) - transform.position).normalized;
-        RaycastHit[] hits = Physics.RaycastAll(transform.position, direction, 37f,
-                            1 << LayerMask.NameToLayer("Obstacle"));
-        Debug.DrawRay(transform.position, direction * 100f, Color.green);
-        for (int i = 0; i < hits.Length; i++)
+        try
         {
-            ObstacleController[] obj = hits[i].transform.GetComponentsInChildren<ObstacleController>();
-
-            for (int j = 0; j < obj.Length; j++)
+            Vector3 direction = (SpawnManager.Instance.playerTransform.position - new Vector3(0, 1f, 0) - transform.position).normalized;
+            RaycastHit[] hits = Physics.RaycastAll(transform.position, direction, 37f,
+                                1 << LayerMask.NameToLayer("Obstacle"));
+            Debug.DrawRay(transform.position, direction * 100f, Color.green);
+            for (int i = 0; i < hits.Length; i++)
             {
-                obj[j]?.BecomeTransparent();
+                ObstacleController[] obj = hits[i].transform.GetComponentsInChildren<ObstacleController>();
+
+                for (int j = 0; j < obj.Length; j++)
+                {
+                    obj[j]?.BecomeTransparent();
+                }
             }
         }
+        catch(MissingReferenceException)
+        {
+            // 케릭터 죽은 후 에러
+        }
+
     }
 }
