542304716696170e42476e622cb4ccf1a9ca5da0
hwanny1011 <86943651+Hehwanny@users.noreply.github.com>
2023-05-23 22:43:59 +0900

    Remove : Plugin
    #159
    - 플러그인 삭제
    + Json 구조 List 전환 성공
    + List JSon 구조 전환 성공

--- MarstoEarth/Assets/Scripts/OutGames/ReportCheckUI.cs
+++ MarstoEarth/Assets/Scripts/OutGames/ReportCheckUI.cs
@@ -1,30 +1,91 @@
 using UnityEngine;
 using UnityEngine.UI;
-using SimpleJSON;
+using System.Collections.Generic;
+using System.IO;
+using System;
 
+
+[Serializable]
+public class ClearReport
+{
+    int clearRooms;
+    float clearTimes;
+    public int ClearRoom
+    {
+        get { return clearRooms; }
+        set { clearRooms = value; }
+    }
+    public float ClearTime
+    {
+        get { return clearTimes; }
+        set { clearTimes = value; }
+    }
+    public ClearReport()
+    {
+    }
+    public ClearReport(int _clearRooms, float _clearTimes)
+    {
+        clearRooms = _clearRooms;
+        clearTimes = _clearTimes;
+    }
+}
+[Serializable]
+public class Serialization<T>
+{
+    [SerializeField]
+    List<T> _t;
+    public List<T> ToList() { return _t; }
+    public Serialization(List<T> _tmp)
+    {
+        _t = _tmp;
+    }
+}
 public class ReportCheckUI : MonoBehaviour
 {
     public Scrollbar reportScroll;
-    int recordClearRoom;
     public TMPro.TextMeshProUGUI reportclearRoom;
     public TMPro.TextMeshProUGUI reportclearTime;
+    public GameObject reportGameClone;
+    public Transform cloneParents;
+    List<ClearReport> clearReportList;
 
+    private void Awake()
+    {
+        clearReportList = new List<ClearReport>();
+    }
     void Start()
     {
-        TextAsset txtAsset = Resources.Load<TextAsset>("Jsons/TestJson2");
-        JSONNode root = JSON.Parse(txtAsset.text);
-        JSONNode data1 = root["D1"];
-        int score = data1["Score"].AsInt;
-        string name = data1["userID"].Value;
-        reportclearRoom.text = score.ToString();
-        reportclearTime.text = name;
+        for (int i = 0; i < 5; i++)
+        {
+            ClearReport clearReport = new ClearReport();
+            clearReport.ClearRoom = i; //InGameManager.clearedBossRoom + InGameManager.clearedBossRoom;
+            clearReport.ClearTime = i; //Time.deltaTime;
+            clearReportList.Add(clearReport);
+        }
+        // List -> Json
+        string jsonDataList = JsonUtility.ToJson(new Serialization<ClearReport>(clearReportList));
+        using (StreamWriter sr = new StreamWriter(Application.dataPath + "/" + "ClearDatas.json"))
+        {
+            sr.WriteLine(jsonDataList);
+            sr.Close();
+        }
+        // Json -> List
+        List<ClearReport> clearReportToList = JsonUtility.FromJson<Serialization<ClearReport>>(jsonDataList).ToList();
+        foreach(ClearReport one in clearReportToList)
+        {
+            reportclearRoom.text = one.ClearRoom.ToString();
+            reportclearTime.text = one.ClearTime.ToString();
+        }
+        for (int i = 0; i < clearReportList.Count; i++)
+        {
+            GameObject newObject = Instantiate(reportGameClone, cloneParents);
+            // 추가적인 설정이 필요한 경우 newObject를 조작합니다.
+        }
     }
 
     public void ReportGame()
     {
         gameObject.SetActive(true);
-        recordClearRoom = InGameManager.clearedRooms + InGameManager.clearedBossRoom;
-        // reportText.text = "Clear Room = " + recordClearRoom + "\n" + "Clear Time = " + Time.realtimeSinceStartup;
     }
 
     void Update()
