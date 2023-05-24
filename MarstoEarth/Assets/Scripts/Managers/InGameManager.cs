506e6525906da4399069998c0e0d558bf35d11ff
hwanny1011 <86943651+Hehwanny@users.noreply.github.com>
2023-05-24 13:13:53 +0900

    Fix : CardSystem
    #163
    => CardUIControll

--- MarstoEarth/Assets/Scripts/Managers/InGameManager.cs
+++ MarstoEarth/Assets/Scripts/Managers/InGameManager.cs
@@ -42,7 +42,11 @@ public class InGameManager : Singleton<InGameManager>
     public void OnRoomCleared()
     {
         clearedRooms++;
-        if (clearedRooms % 2 == 0) // 2 개의 방을 클리어했을 때
+        //if (clearedRooms % 2 == 0) // 2 개의 방을 클리어했을 때
+        //{
+        //    TriggerEvent();
+        //}
+        if(clearedBossRoom % 1 == 0)
         {
             TriggerEvent();
         }
