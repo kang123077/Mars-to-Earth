576932809e810520f1b4d5de22a8f7995370ad17
MJ <umameosuma@gmail.com>
2023-06-01 15:08:11 +0900

    Add : SaveDelete UI
    #191
    ~ SaveDelete (choice, all) 정상작동 추가
    ~ 씬 백업 추가

--- MarstoEarth/Assets/Scripts/OutGames/ReportCheckPPUI.cs
+++ MarstoEarth/Assets/Scripts/OutGames/ReportCheckPPUI.cs
@@ -5,17 +5,22 @@ using UnityEngine.UI;
 
 public class ReportCheckPPUI : MonoBehaviour
 {
-    public List<PlayerSaveInfo> playerSaveInfoList;
     public List<ReportContentUIController> reportContentList;
     public GameObject reportGameContent;
     public Transform cloneParentsTF;
     public Scrollbar reportScroll;
+    private List<PlayerSaveInfo> playerSaveInfoList;
     private int saveCount;
     private float scrollInput;
 
     private void Awake()
     {
         playerSaveInfoList = LoadPlayerSaveInfo();
+    }
+
+    private void Start()
+    {
+        InitContent();
     }
 
     private List<PlayerSaveInfo> LoadPlayerSaveInfo()
@@ -47,7 +52,7 @@ public class ReportCheckPPUI : MonoBehaviour
         return saveInfoList;
     }
 
-    void Start()
+    private void InitContent()
     {
         foreach (PlayerSaveInfo playerSaveInfo in playerSaveInfoList)
         {
@@ -56,19 +61,71 @@ public class ReportCheckPPUI : MonoBehaviour
             reportContent.InitContent(playerSaveInfo);
             reportContentList.Add(reportContent);
         }
-        playerSaveInfoList.Clear();
     }
 
     public void DataChoiceDelete()
     {
+        List<PlayerSaveInfo> infoToRemove = new List<PlayerSaveInfo>();
+        List<ReportContentUIController> contentToRemove = new List<ReportContentUIController>();
+        for (int i = 0; i < reportContentList.Count; i++)
+        {
+            if (reportContentList[i].contentToggle.isOn)
+            {
+                infoToRemove.Add(playerSaveInfoList[i]);
+                contentToRemove.Add(reportContentList[i]);
+            }
+        }
+        // PlayerSaveInfo 체크
+        foreach (PlayerSaveInfo removeInfo in infoToRemove)
+        {
+            playerSaveInfoList.Remove(removeInfo);
+        }
+        // ReportContentUIController 체크
+        foreach (ReportContentUIController removeContent in contentToRemove)
+        {
+            Destroy(removeContent.gameObject);
+            reportContentList.Remove(removeContent);
+        }
+        // 오염을 막기 위해 먼저 한번 다 지워 줌
+        PlayerPrefs.DeleteAll();
+        saveCount = 0;
+        // 남은 playerSaveInfo 다시 저장해줌
+        foreach (PlayerSaveInfo playerSaveInfo in playerSaveInfoList)
+        {
+            SavePPContent(playerSaveInfo);
+        }
+        PlayerPrefs.Save();
+    }
+
+    public void SavePPContent(PlayerSaveInfo playerSaveInfo)
+    {
+        // JSON 형식으로 직렬화
+        string jsonData = JsonUtility.ToJson(playerSaveInfo, true);
+        // 지금까지 save들의 수 확인, 없으면 0
+        if (PlayerPrefs.HasKey("saveCount"))
+            saveCount = PlayerPrefs.GetInt("saveCount");
+        else
+            saveCount = 0;
+        // 현재 Save Idx에 저장
+        // ex : save0, save1, save2
+        PlayerPrefs.SetString("save" + saveCount.ToString(), jsonData);
+        // Save를 하나 추가
+        PlayerPrefs.SetInt("saveCount", saveCount + 1);
     }
 
     public void DataAllDelete()
     {
+        foreach (ReportContentUIController reportContent in reportContentList)
+        {
+            Destroy(reportContent.gameObject);
+        }
+        reportContentList.Clear();
+        PlayerPrefs.DeleteAll();
     }
 
     public void ReportGame()
     {
+        // Title Button UI에서 사용
         gameObject.SetActive(true);
     }
 
