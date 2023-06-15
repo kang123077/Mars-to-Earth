0731a65c11751191943b301cb98e0be02f7140b0
hwanny1011 <86943651+Hehwanny@users.noreply.github.com>
2023-06-15 15:11:14 +0900

    Add : Version Check
    #212
    + 버젼 체크 관련 스크립팅
    + 에임 비주얼 체인지

--- MarstoEarth/Assets/Scripts/Managers/OutGameManager.cs
+++ MarstoEarth/Assets/Scripts/Managers/OutGameManager.cs
@@ -1,11 +1,23 @@
 using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine;
 
 public class OutGameManager : Singleton<OutGameManager>
 {
+    public string URL = ""; // 버전체크를 위한 URL
+    public string CurVersion; // 현재 빌드버전
+    string latsetVersion; // 최신버전
+    public GameObject newVersionAvailable; // 버전확인 UI
+
     protected override void Awake()
     {
         base.Awake();
         OutGameInit();
+    }
+
+    void Start()
+    {
+        StartCoroutine(LoadTxtData(URL));
     }
 
     public void SceneChage(string sceneName)
@@ -17,5 +29,39 @@ public class OutGameManager : Singleton<OutGameManager>
     {
         MapInfo.ResetValues();
         Character.staticStat.ResetValues();
+    }
+
+    public void VersionCheck()
+    {
+        if (CurVersion != latsetVersion)
+        {
+            newVersionAvailable.SetActive(true);
+        }
+        else
+        {
+            newVersionAvailable.SetActive(false);
+        }
+    }
+
+
+    IEnumerator LoadTxtData(string url)
+    {
+        UnityEngine.Networking.UnityWebRequest www = UnityEngine.Networking.UnityWebRequest.Get(url);
+        yield return www.SendWebRequest(); // 페이지 요청
+
+        if (www.result == UnityEngine.Networking.UnityWebRequest.Result.ConnectionError || www.result == UnityEngine.Networking.UnityWebRequest.Result.ProtocolError)
+        {
+            Debug.Log("error get page");
+        }
+        else
+        {
+            latsetVersion = www.downloadHandler.text; // 웹에 입력된 최신버전
+        }
+        VersionCheck();
+    }
+
+    public void OpenURL(string url) // 스토어 열기
+    {
+        Application.OpenURL(url);
     }
 }
