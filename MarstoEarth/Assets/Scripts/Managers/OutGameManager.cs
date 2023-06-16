b15a57e01c5dd97d6dfc255fc7588e3d99e6c748
hwanny1011 <86943651+Hehwanny@users.noreply.github.com>
2023-06-16 09:49:09 +0900

    Add : InternetCheckUI
    + 인터넷 체크 UI
    + 인터넷 미연결시 버전 미확인

--- MarstoEarth/Assets/Scripts/Managers/OutGameManager.cs
+++ MarstoEarth/Assets/Scripts/Managers/OutGameManager.cs
@@ -8,6 +8,7 @@ public class OutGameManager : Singleton<OutGameManager>
     public string CurVersion; // 현재 빌드버전
     string latsetVersion; // 최신버전
     public GameObject newVersionAvailable; // 버전확인 UI
+    public GameObject internetCheck; // 인터넷 연결 UI
 
     protected override void Awake()
     {
@@ -48,16 +49,18 @@ public class OutGameManager : Singleton<OutGameManager>
     {
         UnityEngine.Networking.UnityWebRequest www = UnityEngine.Networking.UnityWebRequest.Get(url);
         yield return www.SendWebRequest(); // 페이지 요청
-
+       
         if (www.result == UnityEngine.Networking.UnityWebRequest.Result.ConnectionError || www.result == UnityEngine.Networking.UnityWebRequest.Result.ProtocolError)
         {
             Debug.Log("error get page");
+            internetCheck.SetActive(true);
         }
         else
         {
             latsetVersion = www.downloadHandler.text; // 웹에 입력된 최신버전
+            internetCheck.SetActive(false);
+            VersionCheck();
         }
-        VersionCheck();
     }
 
     public void OpenURL(string url) // 스토어 열기
